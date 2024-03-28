07a76032003c327d4ea72a0a54e6119e3d290bcc
SemRadoes <semradijs@hotmail.com>
2024-03-28 23:39:39 +0100

    having added a new page and got the respective data for it.

--- InfoTvShow.js
+++ InfoTvShow.js
@@ -1,10 +1,11 @@
 const searchParams = new URLSearchParams(window.location.search);
 const showID = searchParams.get('id');
-alert(showID);
 const baseURL = `https://api.tvmaze.com/shows/`;
-const showOfID = async () =>{
+const showOfID = async () => {
     const overview = await axios.get(`${baseURL}${showID}`);
     const res = overview.data;
     console.log(res);
-}
-showOfID();
+    document.querySelector('title').innerHTML = `${res.name}`;
+    
+};
+
