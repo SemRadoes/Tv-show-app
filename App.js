f18e4f32bf7dc82f3c31107a86eddf7ebbb58874
SemRadoes <semradijs@hotmail.com>
2024-04-18 22:31:01 +0200

    selects refurbished

--- App.js
+++ App.js
@@ -25,6 +25,30 @@ const createShowElements = (element, className, content) => {
     }
     return para;
 };
+const showsWithFiltering = (arg, element) => {
+    const filteredShow = document.createElement('div');
+    filteredShow.classList.add('show');
+    const name = createShowElements('div', 'name', `${arg.name}`);
+    const genres = createShowElements('div', 'genres', `${arg.genres}`);
+    const image = createShowElements('img', 'showPoster', `${arg.image}`);
+    const rating = createShowElements('div', 'rating', `${arg.rating}`);
+    const premiered = createShowElements('div', 'premiered', `${arg.premiered}`);
+    const nameRating = document.createElement('div');
+    nameRating.classList.add('nameRating');
+    nameRating.appendChild(name);
+    nameRating.appendChild(rating);
+    filteredShow.appendChild(image);
+    filteredShow.appendChild(nameRating);
+    filteredShow.appendChild(genres);
+    filteredShow.appendChild(premiered);
+    element.appendChild(filteredShow);
+    filteredShow.addEventListener("mouseover", () => {
+        filteredShow.classList.add("movieScladeOnMouseOver");
+    });
+    filteredShow.addEventListener("click", () => {
+        location.href=`/Tv Show Info.html?id=${arg.id}`;
+    });
+}
 const shows = async () => {
     const overview = await axios.get(baseURL);
     console.log(overview.data);
@@ -87,34 +111,14 @@ const createDropdowns = async (keyword) => {
             option.innerHTML = genre;
             select.append(option);
         }
-    };
-    if(keyword === "language"){
-        let languages = [];
-        for( let showLanguage of res){
-            if(!languages.includes(showLanguage.language)){
-                languages.push(showLanguage.language);
-            }
-        }
-        console.log(languages);
-        const select = document.querySelector('#language');
-        for ( let language of languages){
-            const option = document.createElement('option');
-            option.value = language;
-            option.innerHTML = language;
-            select.append(option);
-        }
-    };
+    }; 
 }
 
-const filterShows = async (keyword) => {
+const filterShows = async () => {
     const overview = await axios.get(baseURL);
     const res = overview.data;
-    if(keyword === "genre"){
-        if($('#genre').val() === "default"){
-            const tvShows = document.querySelector('#showlist');
-            $('#showlist').empty();
     const list = res.map(element => {
-        return showList ={
+        return {
             id: element.id,
             name: element.name,
             genres: element.genres,
@@ -125,69 +129,17 @@ const filterShows = async (keyword) => {
             premiered: element.premiered.slice(0,4),
         };
     });
-    list.forEach(element => {
-        const show = document.createElement('div');
-        show.classList.add('show');
-        const name = createShowElements('div', 'name', `${element.name}`);
-        const genres = createShowElements('div', 'genres', `${element.genres}`);
-        const image = createShowElements('img', 'showPoster', `${element.image}`);
-        const rating = createShowElements('div', 'rating', `${element.rating}`);
-        const premiered = createShowElements('div', 'premiered', `${element.premiered}`);
-        const nameRating = document.createElement('div');
-        nameRating.classList.add('nameRating');
-        nameRating.appendChild(name);
-        nameRating.appendChild(rating);
-        show.appendChild(image);
-        show.appendChild(nameRating);
-        show.appendChild(genres);
-        show.appendChild(premiered);
-        tvShows.appendChild(show);
-        show.addEventListener("mouseover", () => {
-            show.classList.add("movieScladeOnMouseOver");
-        });
-        show.addEventListener("click", () => {
-            location.href=`/Tv Show Info.html?id=${element.id}`;
-        });
-    });
-        } else {
-            const genre = $('#genre').val();
-            console.log(genre);
-            const tvShows = document.querySelector('#showlist');
-            $('#showlist').empty();
-            for (let show of res){
-                if(show.genres.includes(genre)){
-                    const filteredShow = document.createElement('div');
-                    filteredShow.classList.add('show');
-                    const name = createShowElements('div', 'name', `${show.name}`);
-                    const genres = createShowElements('div', 'genres', `${show.genres}`);
-                    const image = createShowElements('img', 'showPoster', `${show.image.medium}`);
-                    const rating = createShowElements('div', 'rating', `${show.rating.average}`);
-                    const premiered = createShowElements('div', 'premiered', `${show.premiered}`);
-                    const nameRating = document.createElement('div');
-                    nameRating.classList.add('nameRating');
-                    nameRating.appendChild(name);
-                    nameRating.appendChild(rating);
-                    filteredShow.appendChild(image);
-                    filteredShow.appendChild(nameRating);
-                    filteredShow.appendChild(genres);
-                    filteredShow.appendChild(premiered);
-                    tvShows.appendChild(filteredShow);
-                    filteredShow.addEventListener("mouseover", () => {
-                        filteredShow.classList.add("movieScladeOnMouseOver");
-                    });
-                    filteredShow.addEventListener("click", () => {
-                        location.href=`/Tv Show Info.html?id=${show.id}`;
-                    });
-                }
-            }
-        }
-    };
-    if(keyword === "language"){
-        
-    };
-    if(keyword === "rating"){
-        
-    };
-    
+    $('#showlist').empty();
+    const genre = $('#genre').val();
+    const rating = $('#rating').val();
+    const tvShows = document.querySelector('#showlist');
+    for (let show of list){
+        if(show.genres.includes(genre) && show.rating >= rating){
+            showsWithFiltering(show, tvShows);
+        } else if(show.rating >= rating && genre === "default"){
+            showsWithFiltering(show, tvShows);
+        } else if(rating === "default" && show.genres.includes(genre))
+            showsWithFiltering(show, tvShows);
+    }
 }
 
